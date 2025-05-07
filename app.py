1d868eb93d80c6a3f43f7d8352411b7ef36cd23c
Michał <michal.grysinski@dsv.com>
2025-05-07 10:58:14 +0200

    pierwsza funkcja getuserbyphone

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, url_for, request
 import subprocess
+import json
+import time
 
 #STATIC VARIABLE
 
@@ -14,11 +16,31 @@ def index():
 def PullMembersOfAgroup():
     return render_template('PullMembersOfAgroup.html')
 
-@app.route('/GetUserByPhoneNumber')
+@app.route('/GetUserByPhoneNumber', methods=['GET', 'POST'])
 def GetUserByPhoneNumber():
+
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        phone_prefix = request.form.get('phone_prefix')
+        phone_number = request.form.get('phone_number')
+        full_number = f'{phone_prefix}{phone_number}'
+
+        data = get_user_by_number(full_number)
+
+        return render_template(
+            'GetUserByPhoneNumber.html',
+            username=username,
+            password=password,
+            phone_prefix=phone_prefix,
+            phone_number=phone_number,
+            result=data
+        )
     return render_template('GetUserByPhoneNumber.html')
 
-@app.route('/GetAllGroupsaUserIsaMemberOf')
+       
+
+@app.route('/GetAllGroupsaUserIsaMemberOf', methods=['GET', 'POST'])
 def GetAllGroupsaUserIsaMemberOf():
     return render_template('GetAllGroupsaUserIsaMemberOf.html')
 
@@ -29,6 +51,25 @@ def GetChangeHistoryOfaGivenUser():
 @app.route('/GetChangeHistoryOfaGivenGroup')
 def GetChangeHistoryOfaGivenGroup():
     return render_template('GetChangeHistoryOfaGivenGroup.html')
+
+
+def get_user_by_number(phone_number):
+    try:
+        result = subprocess.run(["powershell", "-ExecutionPolicy", "Bypass", "-File", ".\\Scripts\\Get-UserByNumber.ps1", phone_number], capture_output=True, text=True, timeout=30)
+
+        if result.returncode != 0:
+            print(f'Error {result.stderr}')
+            return None
+        print(json.loads(result.stdout))
+        return json.loads(result.stdout)
+    
+    except subprocess.TimeoutExpired:
+        print("Skrypt przekroczył limit czasu.")
+        return None
+    except json.JSONDecodeError:
+        print("Nie udało się zdekodować JSON-a.")
+        print("Odpowiedź:", result.stdout)
+        return None
 
 if __name__ == "__main__":
     app.run(debug=True)
